854068108f0e3c203cd66b968bb9133d7d43525d
iremumut <irem2000.nur@gmail.com>
2022-02-01 00:47:03 +0300

    components made

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
-// Code Keypad Component Here
+function Keypad(){
+    function inputHandler(){
+        console.log("Entering password...")
+    }
+    return (
+        <diV>
+            <input type="password" onChange={inputHandler}></input>
+        </diV>
+    )
+}
+
+export default Keypad;
